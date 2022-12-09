4635e2ac3ef7d214aa5e1d850cd3497c2d9dd349
Mackenzie Gray <mdkulen@gmail.com>
2022-12-09 16:38:52 +1100

    edit script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// ---------- Definition of variables ---------- //
-
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
-
 // ---------- Definition of class Calculator ---------- //
 
 class Calculator {
@@ -115,6 +104,17 @@ class Calculator {
         };
     };
 };
+
+// ---------- Definition of variables ---------- //
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 // ---------- Event listeners for all buttons ---------- //
 
